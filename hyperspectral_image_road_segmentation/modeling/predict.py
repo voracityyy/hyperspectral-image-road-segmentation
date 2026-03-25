0af82db516aeb4a6457922feb0736977f616f684
Josh <joshpenaojas@outlook.com>
2026-03-24 20:17:38 -0700

    Increased batch_size; Increased epochs; Added early stopping with patience=10; Added basic plots; Added some testing code

--- hyperspectral_image_road_segmentation/modeling/predict.py
+++ hyperspectral_image_road_segmentation/modeling/predict.py
@@ -4,6 +4,10 @@ from loguru import logger
 from tqdm import tqdm
 import typer
 
+from torch.utils.data import DataLoader, random_split
+
+from hyperspectral_image_road_segmentation.dataset import LoadHyperspectralImages 
+
 from hyperspectral_image_road_segmentation.config import MODELS_DIR, PROCESSED_DATA_DIR
 
 app = typer.Typer()
@@ -11,20 +15,40 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "test_features.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    predictions_path: Path = PROCESSED_DATA_DIR / "test_predictions.csv",
-    # -----------------------------------------
+    # # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
+    # features_path: Path = PROCESSED_DATA_DIR / "test_features.csv",
+    # model_path: Path = MODELS_DIR / "model.pkl",
+    # predictions_path: Path = PROCESSED_DATA_DIR / "test_predictions.csv",
+    # # -----------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Performing inference for model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Inference complete.")
-    # -----------------------------------------
+    # # ---- REPLACE THIS WITH YOUR OWN CODE ----
+    # logger.info("Performing inference for model...")
+    # for i in tqdm(range(10), total=10):
+    #     if i == 5:
+    #         logger.info("Something happened for iteration 5.")
+    # logger.success("Inference complete.")
+    # # -----------------------------------------
 
+
+    tensor = LoadHyperspectralImages()
+    total_size = len(tensor)
+    train_size = int(0.8 * total_size)
+    validation_size = int(0.1 * total_size)
+    # ^ boilerplate code so we can get the exact test dataset size
+    test_size = int(total_size - train_size - validation_size) # aka the remainder
+
+    test_ds = random_split(tensor, [test_size])
+
+
+    # Possible evaluation metrics to consider:
+        # Intersection over Union (IoU)
+        # Dice Coefficient
+        # Precision
+        # Recall
+        # F1 Score
+        # Mean Absolute Error (MAE)
+        # Hausdorff Distance
+        # Pixel Accuracy
 
 if __name__ == "__main__":
     app()
--- hyperspectral_image_road_segmentation/modeling/train.py
+++ hyperspectral_image_road_segmentation/modeling/train.py
@@ -32,11 +32,10 @@ def main(
     total_size = len(tensor)
     train_size = int(0.8 * total_size)
     validation_size = int(0.1 * total_size)
-    test_size = int(total_size - train_size - validation_size) # aka the remainder
-    train_ds, validation_ds, test_ds = random_split(tensor, [train_size, validation_size, test_size])
+    train_ds, validation_ds = random_split(tensor, [train_size, validation_size])
     
-    train_loader = DataLoader(train_ds, batch_size=10, shuffle=True)
-    val_loader = DataLoader(validation_ds, batch_size=10, shuffle=True)
+    train_loader = DataLoader(train_ds, batch_size=16, shuffle=True)
+    val_loader = DataLoader(validation_ds, batch_size=16, shuffle=false)
 
     # Use GPU if available
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -47,6 +46,9 @@ def main(
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001) # still need to test which param values are best
     # Calculates model prediction errors
     loss_function = smp.losses.DiceLoss(mode="binary") # still need to test which loss function is best
+
+    # loss_function = smp.losses.CrossEntropyLoss()
+
     # Reduced learning rate (lr in optimizer) after x epochs (patience value)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
         optimizer, mode="min", factor=0.5, patience=3 # still need to test which param values are best
@@ -61,13 +63,17 @@ def main(
     # Track the weights from best performing epoch
     best_state = None
     
-    
     # ---- TRAINING LOOP ----
-    for epoch in range(1, 10):
+    for epoch in range(0, 100):
 
         model.train()
         train_loss_total = 0
         train_sample_total = 0
+
+        # If there is no validation loss improvement after 10 epochs,
+        # stop training to prevent overfitting
+        no_improvement_count = 0
+        patience = 10
 
         for hyperspectrals, masks in train_loader:
             hyperspectrals = hyperspectrals.to(device)
@@ -108,12 +114,17 @@ def main(
 
         scheduler.step(val_loss_avg)
 
-        logger.info(f"epoch {epoch:02d} train_loss_avg={train_loss_avg:.4f} val_loss_avg={val_loss_avg:.4f}")
+        logger.info(f"epoch {epoch+1:02d} train_loss_avg={train_loss_avg:.4f} val_loss_avg={val_loss_avg:.4f}")
 
         # Save model
         if val_loss_avg < best_val_loss:
             best_val_loss = val_loss_avg
             best_state = model.state_dict()
+        else: 
+            no_improvement_count += 1
+
+        if no_improvement_count >= patience:
+            break
 
     MODELS_DIR.mkdir(parents=True, exist_ok=True)
     torch.save(best_state, model_path)
